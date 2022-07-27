bdb3fecc89009324eb6172e7e1a1435da3091e7d
Diyu Hu <diyuhu@uvic.ca>
2022-07-27 08:36:46 -0700

    Formatted PaymentChannel.sol. Added test for payment channel.

--- contracts/PaymentChannel.sol
+++ contracts/PaymentChannel.sol
@@ -6,85 +6,85 @@ pragma experimental ABIEncoderV2;
 import "@openzeppelin/contracts/utils/cryptography/ECDSA.sol";
 
 contract PaymentChannel {
-    using ECDSA for bytes32;
+  using ECDSA for bytes32;
 
-    event StartSenderClose();
-    event Withdraw(uint256 amountAuthorized, bytes signature);
+  event StartSenderClose();
+  event Withdraw(uint256 amountAuthorized, bytes signature);
 
-    address payable public sender;
-    address payable public receiver;
-    uint256 public withdrawnAmount;
-    uint public closeDuration;
-    uint public expiration = type(uint).max;
+  address payable public sender;
+  address payable public receiver;
+  uint256 public withdrawnAmount;
+  uint public closeDuration;
+  uint public expiration = type(uint).max;
 
-    constructor(address payable receiver_, uint closeDuration_) payable {
-        sender = payable(msg.sender);
-        receiver = receiver_;
-        closeDuration = closeDuration_;
+  constructor(address payable receiver_, uint closeDuration_) payable {
+    sender = payable(msg.sender);
+    receiver = receiver_;
+    closeDuration = closeDuration_;
+  }
+
+  function getMessageHash(uint amount) external view returns (bytes32) {
+    return getMessageHash_(amount);
+  }
+
+  function getEthSignedMessageHash(uint amount) external view returns (bytes32) {
+    return getEthSignedMessageHash_(amount);
+  }
+
+  function verifyMessage(uint amount, bytes memory signature) external view returns (bool) {
+    return verifyMessage_(amount, signature);
+  }
+
+  function close(uint amount, bytes memory signature) external {
+    require(msg.sender == receiver, "Only receiver can call close function.");
+    require(verifyMessage_(amount, signature), "Signed message is invalid.");
+    require(amount >= withdrawnAmount, "Amount must be greater than or equal to withdrawn amount.");
+
+    if (amount > withdrawnAmount) {
+      (bool success, ) = receiver.call{value : amount - withdrawnAmount}("");
+      require(success, "Transaction failed.");
     }
 
-    function getMessageHash(uint amount) external view returns (bytes32) {
-        return getMessageHash_(amount);
-    }
+    selfdestruct(sender);
+  }
 
-    function getEthSignedMessageHash(uint amount) external view returns (bytes32) {
-        return getEthSignedMessageHash_(amount);
-    }
+  function startSenderClose() public {
+    require(msg.sender == sender, "Only sender can start sender close.");
+    expiration = block.timestamp + closeDuration;
+    emit StartSenderClose();
+  }
 
-    function verifyMessage(uint amount, bytes memory signature) external view returns (bool) {
-        return verifyMessage_(amount, signature);
-    }
+  function claimTimeout() public {
+    require(block.timestamp >= expiration, "Contract is not expired yet.");
+    selfdestruct(sender);
+  }
 
-    function close(uint amount, bytes memory signature) external {
-        require(msg.sender == receiver, "Only receiver can call close function.");
-        require(verifyMessage_(amount, signature), "Signed message is invalid.");
-        require(amount >= withdrawnAmount, "Amount must be greater than or equal to withdrawn amount.");
+  function deposit() public payable {
+    require(msg.sender == sender, "Only sender can deposit eth.");
+  }
 
-        if (amount > withdrawnAmount) {
-            (bool sent, ) = receiver.call{value : amount - withdrawnAmount}("");
-            require(sent);
-        }
+  function withdraw(uint authorizedAmount, bytes memory signature) public {
+    require(msg.sender == receiver, "Only receiver withdraw.");
+    require(verifyMessage_(authorizedAmount, signature), "Signed message is invalid.");
+    require(authorizedAmount > withdrawnAmount, "Authorized amount must be greater than withdrawn amount.");
 
-        selfdestruct(sender);
-    }
+    uint256 amount = authorizedAmount - withdrawnAmount;
+    withdrawnAmount += amount;
+    (bool success, ) = receiver.call{value : amount}("");
+    require(success, "Transaction failed.");
 
-    function startSenderClose() public {
-        require(msg.sender == sender, "Only sender can start sender close.");
-        expiration = block.timestamp + closeDuration;
-        emit StartSenderClose();
-    }
+    emit Withdraw(authorizedAmount, signature);
+  }
 
-    function claimTimeout() public {
-        require(block.timestamp >= expiration, "Contract is not expired yet.");
-        selfdestruct(sender);
-    }
+  function getMessageHash_(uint amount) private view returns (bytes32) {
+    return keccak256(abi.encodePacked(address(this), amount));
+  }
 
-    function deposit() public payable {
-        require(msg.sender == sender, "Only sender can deposit eth.");
-    }
+  function getEthSignedMessageHash_(uint amount) private view returns (bytes32) {
+    return getMessageHash_(amount).toEthSignedMessageHash();
+  }
 
-    function withdraw(uint256 authorizedAmount, bytes memory signature) public {
-        require(msg.sender == receiver, "Only receiver withdraw.");
-        require(verifyMessage_(authorizedAmount, signature), "Signed message is invalid.");
-        require(authorizedAmount > withdrawnAmount, "Authorized amount must be greater than withdrawn amount.");
-
-        uint256 amount = authorizedAmount - withdrawnAmount;
-        withdrawnAmount += amount;
-        (bool sent, ) = receiver.call{value : amount}("");
-        require(sent);
-
-        emit Withdraw(authorizedAmount, signature);
-    }
-
-    function getMessageHash_(uint amount) private view returns (bytes32) {
-        return keccak256(abi.encodePacked(address(this), amount));
-    }
-
-    function getEthSignedMessageHash_(uint amount) private view returns (bytes32) {
-        return getMessageHash_(amount).toEthSignedMessageHash();
-    }
-
-    function verifyMessage_(uint amount, bytes memory signature) private view returns (bool) {
-        return getEthSignedMessageHash_(amount).recover(signature) == sender;
-    }
+  function verifyMessage_(uint amount, bytes memory signature) private view returns (bool) {
+    return getEthSignedMessageHash_(amount).recover(signature) == sender;
+  }
 }
--- test/PaymentChannel.js
+++ test/PaymentChannel.js
@@ -1,11 +1,113 @@
-const { expect } = require("chai");
-const { ethers } = require("hardhat");
+const { expect } = require('chai')
+const { ethers } = require('hardhat')
+const { loadFixture, time } = require('@nomicfoundation/hardhat-network-helpers')
 
-describe("PaymentChannel contract", function () {
-    it("Deployment should assign the total supply of tokens to the owner", async function () {
-        const [bob, alice] = await ethers.getSigners();
-        const PaymentChannel = await ethers.getContractFactory("PaymentChannel", bob);
-        const paymentChannel = await PaymentChannel.deploy(alice.address, 3600, {value: "10000000000000000000"});
-        expect(await paymentChannel.sender()).to.equal(bob.address);
-    });
-});
+describe('PaymentChannel', function () {
+  async function deployPaymentChannelFixture () {
+    const valueStr = ethers.utils.parseUnits('10').toString()
+    const closeDuration = 60 * 60
+    const [bob, alice] = await ethers.getSigners()
+    const PaymentChannel = await ethers.getContractFactory('PaymentChannel', bob)
+    const paymentChannel = await PaymentChannel.deploy(alice.address, closeDuration, { value: valueStr })
+    return { paymentChannel, bob, alice }
+  }
+
+  it('getMessageHash, getEthSignedMessageHash, verifyMessage', async function () {
+    const { paymentChannel, bob } = await loadFixture(deployPaymentChannelFixture)
+    const contractAddress = paymentChannel.address
+    const amount = 10
+    const messageHash = ethers.utils.solidityKeccak256(['address', 'uint'], [contractAddress, amount])
+    expect(await paymentChannel.getMessageHash(amount)).to.equal(messageHash)
+
+    const ethSignedMessageHash = ethers.utils.solidityKeccak256(['string', 'bytes32'],
+      ['\x19Ethereum Signed Message:\n32', messageHash])
+    expect(await paymentChannel.getEthSignedMessageHash(amount)).to.equal(ethSignedMessageHash)
+
+    const signature = await bob.signMessage(ethers.utils.arrayify(messageHash))
+    expect(await paymentChannel.verifyMessage(amount, signature))
+  })
+
+  it('withdraw-case1', async function () {
+    const { paymentChannel, bob, alice } = await loadFixture(deployPaymentChannelFixture)
+    const amount = 5
+    const messageHash = await paymentChannel.getMessageHash(amount)
+    const signature = await bob.signMessage(ethers.utils.arrayify(messageHash))
+    await expect(paymentChannel.withdraw(amount, signature)).to.be.revertedWith('Only receiver withdraw.')
+    await expect(paymentChannel.connect(alice).withdraw(amount + 1, signature)).to.be.revertedWith(
+      'Signed message is invalid.')
+    await paymentChannel.connect(alice).withdraw(amount, signature)
+
+    const newAmount = amount - 1
+    const newMessageHash = await paymentChannel.getMessageHash(newAmount)
+    const newSignature = await bob.signMessage(ethers.utils.arrayify(newMessageHash))
+    await expect(paymentChannel.connect(alice).withdraw(newAmount, newSignature)).to.be.revertedWith(
+      'Authorized amount must be greater than withdrawn amount.')
+  })
+
+  it('withdraw-case2', async function () {
+    const { paymentChannel, bob, alice } = await loadFixture(deployPaymentChannelFixture)
+    const amount = ethers.utils.parseUnits('11')
+    const messageHash = await paymentChannel.getMessageHash(amount)
+    const signature = await bob.signMessage(ethers.utils.arrayify(messageHash))
+    await expect(paymentChannel.connect(alice).withdraw(amount, signature)).to.be.revertedWith('Transaction failed.')
+  })
+
+  it('close-case1', async function () {
+    const { paymentChannel, bob, alice } = await loadFixture(deployPaymentChannelFixture)
+    const amount = 10
+    const messageHash = await paymentChannel.getMessageHash(amount)
+    const signature = await bob.signMessage(ethers.utils.arrayify(messageHash))
+    await paymentChannel.connect(alice).close(amount, signature)
+  })
+
+  it('close-case2', async function () {
+    const { paymentChannel, bob, alice } = await loadFixture(deployPaymentChannelFixture)
+    const amount = 5
+    const messageHash = await paymentChannel.getMessageHash(amount)
+    const signature = await bob.signMessage(ethers.utils.arrayify(messageHash))
+    await expect(paymentChannel.close(amount, signature)).to.be.revertedWith('Only receiver can call close function.')
+    await expect(paymentChannel.connect(alice).close(amount + 1, signature)).to.be.revertedWith(
+      'Signed message is invalid.')
+    paymentChannel.connect(alice).withdraw(amount, signature)
+
+    const newAmount = amount - 1
+    const newMessageHash = await paymentChannel.getMessageHash(newAmount)
+    const newSignature = await bob.signMessage(ethers.utils.arrayify(newMessageHash))
+    await expect(paymentChannel.connect(alice).close(newAmount, newSignature)).to.be.revertedWith(
+      'Amount must be greater than or equal to withdrawn amount.')
+
+    await paymentChannel.connect(alice).close(amount, signature)
+  })
+
+  it('close-case3', async function () {
+    const { paymentChannel, bob, alice } = await loadFixture(deployPaymentChannelFixture)
+    const amount = ethers.utils.parseUnits('11')
+    const messageHash = await paymentChannel.getMessageHash(amount)
+    const signature = await bob.signMessage(ethers.utils.arrayify(messageHash))
+    await expect(paymentChannel.connect(alice).close(amount, signature)).to.be.revertedWith('Transaction failed.')
+  })
+
+  it('startSenderClose', async function () {
+    const { paymentChannel, alice } = await loadFixture(deployPaymentChannelFixture)
+    await paymentChannel.startSenderClose()
+    await expect(paymentChannel.connect(alice).startSenderClose()).to.be.revertedWith(
+      'Only sender can start sender close.')
+  })
+
+  it('claimTimeout', async function () {
+    const { paymentChannel } = await loadFixture(deployPaymentChannelFixture)
+    await expect(paymentChannel.claimTimeout()).to.be.revertedWith('Contract is not expired yet.')
+    const amountInSeconds = 60 * 60 * 2
+    await paymentChannel.startSenderClose()
+    await time.increase(amountInSeconds)
+    await paymentChannel.claimTimeout()
+  })
+
+  it('deposit', async function () {
+    const { paymentChannel, alice } = await loadFixture(deployPaymentChannelFixture)
+    const valueStr = ethers.utils.parseUnits('5').toString()
+    await expect(paymentChannel.connect(alice).deposit({ value: valueStr })).to.be.revertedWith(
+      'Only sender can deposit eth.')
+    await paymentChannel.deposit({ value: valueStr })
+  })
+})
